374b21a6df84837622ce9abebde4653903ab6011
Roman <mail@romanbrazhnikov.ru>
2017-11-02 22:10:48 +0700

    Rx added

--- src/main/java/ru/romanbrazhnikov/Main.java
+++ src/main/java/ru/romanbrazhnikov/Main.java
@@ -1,8 +1,11 @@
 package ru.romanbrazhnikov;
 
+import io.reactivex.Flowable;
+
 public class Main {
 
     public static void main(String[] args) {
+        Flowable.just("Starting...").subscribe(System.out::println);
         Tester.testParser();
     }
 }
